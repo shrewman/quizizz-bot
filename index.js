8a567593e395abe3a654feb2985b3f7ba921c2da
FeelsWhiteMan <feelswhiteman@gmail.com>
2023-04-29 16:26:15 +0300

    Refactoring

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
 import puppeteer from "puppeteer";
 
-// TODO: {
-const leaderboardWrapper = '.leaderboard-wrapper';
+// quizziz selectors
+const leaderboardSelector = '.leaderboard-wrapper';
 const powerupSelector = '.powerup-award-container';
-const shouldPowerup = '.apply-now';
+const usePowerupButton = '.apply-now';
 const continueButton = '.right-navigator';
 const submitAnswerButton = '.submit-button';
 
-const transitionerContainer = '.transitioner-container';
-const scorebarContainer = '.scorebar-container';
-const screenRedemption = '.screen-redemption-question-selector';
-const redemptionButtonSelector = '.gradient-container';
+const redemptionSelector = '.screen-redemption-question-selector';
+const redemptionQuestionButton = '.gradient-container';
 
-const firstLevelFeedback = '.first-level-feedback';
-const screenSummarySelector = '.screen-summary';
-
-const gameOverSelector = '.screen-game-over';
-const accuracyInfoSection = '.accuracy-info-section';
+const levelFeedbackSelector = '.first-level-feedback';
 const toSummarySelector = '.skip-summary';
-// TODO: }
+const gameOverSelector = '.screen-game-over';
+const accuracyInfoSelector = '.accuracy-info-section';
 
 const browser = await puppeteer.launch({
     headless: false,
@@ -29,85 +24,139 @@ const browser = await puppeteer.launch({
     }
 });
 
-const getAnswers = async (roomCode) => {
+// quizit selectors
+const quizitInputSelector = 'input[type="text"][placeholder="Pin or Link"]';
+const quizitGetAnswersButton = 'button[type="button"].bg-blue-500';
+const quizitCardSelector = 'div.rounded-xl';
+
+const getAnswersFromQuizit = async (roomCode) => {
     const page = await browser.newPage();
     await page.goto('https://quizit.online/services/quizizz/');
 
-    const inputSelector = 'input[type="text"][placeholder="Pin or Link"]';
-    // await page.type(inputSelector, roomCode);
-    await page.type(inputSelector, 'https://quizizz.com/join/quiz/5e23a5fd4b061d001b80b842/start');
+    await page.type(quizitInputSelector, 'https://quizizz.com/join/quiz/5e23a5fd4b061d001b80b842/start');
+    await page.click(quizitGetAnswersButton);
 
-    const buttonSelector = 'button[type="button"].bg-blue-500';
-    await page.waitForSelector(buttonSelector);
-    await page.click(buttonSelector);
+    await page.waitForSelector(quizitCardSelector);
+    return await extractAnswers(page);
+};
 
-    await page.waitForSelector('div.rounded-xl');
-
+async function extractAnswers(page) {
     return await page.evaluate(async () => {
         const cards = document.querySelectorAll('div.rounded-xl');
 
-        let map = new Map();
-        for (let card of cards) {
-            const question = card.querySelector('div.rounded-xl h5').innerText;
-            const answer = card.querySelector('div.rounded-xl div').innerText;
-            console.log(question + " " + answer);
-            map.set(question, answer);
-        }
-
-        console.log(map);
         return Array.from(cards).map((card) => {
             const question = card.querySelector('div.rounded-xl h5').innerText;
-            const answer = card.querySelector('div.rounded-xl div').innerText;
-            return {question, answer};
+            const answer = card.querySelector('div.rounded-xl div')
+                .innerText.split('\n').filter(str => str !== '');
+            return { question, answer };
         });
     });
-};
+}
 
-const startQuizziz = async (name, roomCode, answers) => {
-    const page = await browser.newPage();
-    await page.goto(`https://quizizz.com/join?gc=${roomCode}`);
-    await page.goto('https://quizizz.com/join/quiz/5e23a5fd4b061d001b80b842/start');
+async function inputName(page, name) {
+    await page.waitForSelector('.enter-name-field');
+    await page.type('.enter-name-field', name);
+}
 
+async function configureQuizziz(page, name) {
     await page.waitForSelector('.toggle-button');
-    await page.evaluate( () => {
+    await page.evaluate(() => {
         const buttons = document.querySelector('.game-settings-list')
             .querySelectorAll('.toggle-button');
         buttons.forEach(button => {
             button.click()
         });
     });
+}
 
-    // await page.waitForSelector('.enter-name-field');
-    // await page.type('.enter-name-field', name);
+async function startGame(page) {
+    const buttonSelector1 = '.start-game';
+    await page.waitForSelector(buttonSelector1);
+    await page.click(buttonSelector1);
 
-    // const buttonSelector1 = '.start-game';
-    // await page.waitForSelector(buttonSelector1);
-    // await page.click(buttonSelector1);
-    //
-    // const buttonSelector2 = '.start-btn';
-    // await page.waitForSelector(buttonSelector2);
-    // await page.click(buttonSelector2);
+    const buttonSelector2 = '.start-btn';
+    await page.waitForSelector(buttonSelector2);
+    await page.click(buttonSelector2);
+}
+
+async function handleAnnoyingPopups(page) {
+    if (await page.$(leaderboardSelector)) {
+        const button = await page.$(continueButton);
+        if (button) {
+            await button.click();
+        }
+        console.log('leaderboard skipped');
+    }
+
+    if (await page.$(powerupSelector)) {
+        const button = await page.$(continueButton);
+        if (button) {
+            await button.click();
+        }
+        console.log('powerup gaining skipped');
+    }
+
+    if (await page.$(usePowerupButton)) {
+        const button = await page.$(usePowerupButton);
+        if (button) {
+            await button.click();
+        }
+        console.log('annoying powerup used');
+    }
+}
+
+async function handleRedemptionQuestions(page) {
+    if (await page.$(redemptionSelector)) {
+        const button = await page.$(redemptionQuestionButton);
+        if (button) {
+            await button.click();
+        }
+        console.log('redemption question picked');
+    }
+}
+
+async function pickAnswer(page, answer) {
+    const options = await page.$$('div.option');
+    for (let i = 0; i < options.length; i++) {
+        const optionText = await options[i].$eval('div.resizeable p', el => el.innerText);
+        if (answer.includes(optionText)) {
+            await options[i].click();
+        }
+    }
+    const button = await page.$(submitAnswerButton);
+    if (button) await button.click();
+}
+
+const initQuizziz = async (name, roomCode, answers) => {
+    const page = await browser.newPage();
+    await page.goto(`https://quizizz.com/join?gc=${roomCode}`);
+    await page.goto(`https://quizizz.com/join/quiz/5e23a5fd4b061d001b80b842/start`);
+
+    await configureQuizziz(page, name);
+    // await inputName(page, name);
+    // await startGame(page);
 
     while(true) {
-        const questionSelector = '#questionText p';
-        let question, answer;
 
         if(await page.$(gameOverSelector)) {
             console.log('done game over')
             break;
         }
 
-        if (await page.$(toSummarySelector)) {
+        if (await page.$(levelFeedbackSelector)) {
             const toSummary = await page.$(toSummarySelector);
             if (toSummary) await toSummary.click();
             console.log('done to summary')
             break;
         }
 
-        if(await page.$(accuracyInfoSection)) {
+        if(await page.$(accuracyInfoSelector)) {
             console.log('done accuracy info')
             break;
         }
+
+        const questionSelector = '#questionText p';
+        let question, answer;
 
         if (await page.$(questionSelector)) {
             question = await page.evaluate(() => {
@@ -118,52 +167,14 @@ const startQuizziz = async (name, roomCode, answers) => {
             answer =  await card ? card.answer : null;
 
             await console.log(question);
-            let multipleAnswers = answer.split('\n').filter(str => str !== '');
-            console.log(multipleAnswers);
+            console.log(answer);
 
-            const options = await page.$$('div.option');
-            for (let i = 0; i < options.length; i++) {
-                const optionText = await options[i].$eval('div.resizeable p', el => el.innerText);
-                if ( multipleAnswers.includes(optionText) ) {
-                    await options[i].click();
-                }
-            }
-
-            const submitButton = await page.$(submitAnswerButton);
-            if (submitButton) await submitButton.click();
+            await pickAnswer(page, answer);
         }
 
-        if (await page.$(leaderboardWrapper)) {
-            const button = await page.$(continueButton);
-            if (button) {
-                await button.click();
-            }
-            console.log('leaderboard skipped');
-        }
 
-        if (await page.$(powerupSelector)) {
-            const button = await page.$(continueButton);
-            if (button) {
-                await button.click();
-            }
-            console.log('powerup gaining skipped');
-        }
-
-        if (await page.$(shouldPowerup)) {
-            const button = await page.$(shouldPowerup);
-            if (button) {
-                await button.click();
-            }
-            console.log('annoying powerup used');
-        }
-
-        if (await page.$(screenRedemption)) {
-            const button = await page.$(redemptionButtonSelector);
-            if (button) {
-                await button.click();
-            }
-            console.log('redemption question picked');
-        }
+        await handleAnnoyingPopups(page);
+        await handleRedemptionQuestions(page);
 
         await new Promise(resolve => setTimeout(resolve, 1200));
     }
@@ -173,9 +184,9 @@ const startQuizziz = async (name, roomCode, answers) => {
 
 let roomCode = '21153327';
 let name = '**';
-let answers = await getAnswers(roomCode);
+let answers = await getAnswersFromQuizit(roomCode);
 console.log(answers);
-await startQuizziz(name, roomCode, answers);
+await initQuizziz(name, roomCode, answers);
 
 
 
